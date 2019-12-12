3daa793c667933cb9ae9ed3f70b1f0e66f32700f
George Aoyagi <aoyagigeorge@gmail.com>
2019-12-12 03:21:53 -0800

    fixed some more urls

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,6 +3,6 @@ from wiki.views import PageListView, PageDetailView
 
 
 urlpatterns = [
-    path('/', PageListView.as_view(), name='wiki-list-page'),
+    path('', PageListView.as_view(), name='wiki-list-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
 ]
